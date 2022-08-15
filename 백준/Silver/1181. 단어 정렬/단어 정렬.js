752984b4e43bbe9cf4a97f732673bd8f90d8ba38
Yiseob <108213210+Yiseob@users.noreply.github.com>
2022-08-15 16:07:07 +0900

    [Silver V] Title: 단어 정렬, Time: 1324 ms, Memory: 23660 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.js
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.js
@@ -1,26 +1,5 @@
-const fs = require("fs");
-const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-let input = fs.readFileSync(filePath).toString().trim().split("\n");
-
-solution(input);
-
-function solution(inputArr) {
-  let answer = [];
-  let arr = inputArr;
-  arr.shift();
-
-  arr = Array.from(new Set(arr)); // 중복제어
-
-  let sorted = arr.sort((a, b) => { // 길이별 정렬
-    if (a.length !== b.length) {
-      return a.length - b.length;
-    }
-  });
-  for (let i = 1; i <= sorted[sorted.length - 1].length; i++) { // 같은 길이에서 사전순으로 정렬
-    let temp = sorted.filter((el) => el.length === i);
-    answer.push(...temp.sort());
-  }
-  for (let word of answer) { // 정답 출력
-    console.log(word);
-  }
-}
+const [n, ...words] = require("fs").readFileSync("/dev/stdin").toString().trim().split("\n");
+Array.from(new Set(words))
+    .sort((a, b) => a > b ? 1 : (a < b ? -1 : 0))
+    .sort((a, b) => a.length - b.length)
+    .forEach(i => console.log(i));
